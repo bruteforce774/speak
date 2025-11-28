90b6705def989fc678d0f787ae6d619e80e9688a
Daniel Andreas Wang <danielaw@getacademy.no>
2025-11-28 10:35:14 +0100

    get datetime string

--- speak.py
+++ speak.py
@@ -5,15 +5,22 @@ from datetime import datetime
 def get_greeting(name):
     current_hour = datetime.now().hour
     
-    # TODO: Add conditional logic here
-    # What comparison operators do we need?
-    # Hint: We need to check if current_hour falls within ranges
-    
     if current_hour >= 5 and current_hour < 12:
         time_of_day = "morning"
-    # ... what comes next?
+    elif current_hour >= 12 and current_hour < 18:
+        time_of_day = "afternoon"
+    elif current_hour >= 18 and current_hour < 22:
+        time_of_day = "evening"
+    else:
+        time_of_day = "night"
     
     return f"Good {time_of_day}, {name}!"
 
-# Test it
 print(get_greeting("Daniel"))
+
+def get_datetime_string():
+    now = datetime.now()
+    return now.strftime("%d %B %I:%M %p")
+
+date_time = get_datetime_string()
+print(date_time)
